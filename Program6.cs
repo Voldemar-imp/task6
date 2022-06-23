21df5cf7e405ba3aaaf01677e5dea3887dd255ae
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-06-23 17:17:45 +0300

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfImages = 52;
+            int picturesInRow = 3;
+            int picturesOutRow;
+            int numberOfRow;
+
+            numberOfRow = numberOfImages / picturesInRow;
+            picturesOutRow = numberOfImages - numberOfRow * picturesInRow;
+
+            Console.WriteLine("Количество рядов картинок - {0}. Картинок вне ряда - {1}", numberOfRow, picturesOutRow);                      
+        }
+    }
+}
